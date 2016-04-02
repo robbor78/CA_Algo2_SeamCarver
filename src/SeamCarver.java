333cbc4dbf5e664526adb4d7049e10d73a01ba55
name <email>
2016-04-02 10:24:59 +0200

    Corner cases.

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,10 +1,16 @@
 import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
+
+    private Picture picture;
+
     // create a seam carver object based on
     // the given picture
     public SeamCarver(Picture picture) {
-
+        if (picture == null) {
+            throw new java.lang.NullPointerException();
+        }
+        this.picture = new Picture(picture);
     }
 
     // current picture
@@ -14,16 +20,17 @@ public class SeamCarver {
 
     // width of current picture
     public int width() {
-        return -1;
+        return picture.width();
     }
 
     // height of current picture
     public int height() {
-        return -1;
+        return picture.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        CheckValidIndices(x, y);
         return Double.NaN;
     }
 
@@ -40,12 +47,29 @@ public class SeamCarver {
     // remove horizontal seam from
     // current picture
     public void removeHorizontalSeam(int[] seam) {
+        if (seam == null) {
+            throw new java.lang.NullPointerException();
+        }
 
     }
 
     // remove vertical seam from
     // current picture
     public void removeVerticalSeam(int[] seam) {
+        if (seam == null) {
+            throw new java.lang.NullPointerException();
+        }
 
+    }
+
+    private void CheckValidIndices(int x, int y) {
+        CheckValidIndex(x, picture.width());
+        CheckValidIndex(y, picture.height());
+    }
+
+    private void CheckValidIndex(int i, int max) {
+        if (i < 0 || i >= max) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
     }
 }
